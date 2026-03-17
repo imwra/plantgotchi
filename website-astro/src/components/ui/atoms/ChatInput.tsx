b923e261b834e22ca8c9646ee70fd5de35df0d5d
willaraujo <willaraujo@will-mbp.macaroni-micro.ts.net>
2026-03-17 01:03:27 -0400

    fix: center send button arrow with SVG icon

--- website-astro/src/components/ui/atoms/ChatInput.tsx
+++ website-astro/src/components/ui/atoms/ChatInput.tsx
@@ -53,7 +53,9 @@ export default function ChatInput({ value, onChange, onSend, onImageClick, place
         )}
         aria-label="Send message"
       >
-        ➤
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" className="block">
+          <path d="M2 2L14 8L2 14V9L10 8L2 7V2Z" fill="currentColor" />
+        </svg>
       </button>
     </div>
   );
